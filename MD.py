36c3272263378ceab6b4f600c28ccab368c0ef71
b0nba <przemobonba@gmail.com>
2026-07-30 16:33:42 +0200

    Initial commit.

---
+++ MD.py
@@ -0,0 +1,8 @@
+# This probably needs to be moved somewhere else
+class m_space:
+    def __init__(self):
+        self.mean = pd.DataFrame()
+        self.cov = pd.DataFrame()
+        self.corr_matrix = pd.DataFrame()
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+def MTS(oa_design, data):
+
+    # Check if given arguments are proper
+    #
+
+    #
+    # Construct m_space (mahalanobis_space) for a normal group and check its validity
+    #
+
+
+    return 0 # what should it return???
